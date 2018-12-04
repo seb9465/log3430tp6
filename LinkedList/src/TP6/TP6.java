02596439301ce04863eb2e485da75ed6b3a68000
Sebastien Cadorette <cado.94@me.com>
2018-12-03 22:49:14 -0500

    Ajout de quelques tests pour la classe LinkedList.

--- LinkedList/src/TP6/TP6.java
+++ LinkedList/src/TP6/TP6.java
@@ -2,6 +2,7 @@ package TP6;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -33,6 +34,7 @@ public class TP6 {
 		listeEntiers1.add(1);
 		listeEntiers1.add(1);
 		listeEntiers1.add(2);
+		listeEntiers1.add(5);
 		
 		listeEntiers2.add(1);
 		listeEntiers2.add(1);
@@ -46,10 +48,125 @@ public class TP6 {
 
 	// ============== TESTS DE LA CLASSE LINKEDLIST ==============
 	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: UNION
+	 * 		- Tri		: Croissant
+	 * La liste chainee devrait avoir des valeurs croissantes.
+	 */
 	@Test
-	public void test() {
-		assertEquals(6, listeEntiers1.size());
+	public void test1() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(1, listeEntiers1, listeEntiers2, true);
+		
+		assertNotNull("Le resultat ne devrait pas etre null.", resultat);
+		assertEquals("Le resultat devrait contenir trois tableaux.", 3, resultat.getSize());
+		
+		ArrayList<Object> resultatUnion = resultat.getAt(2);
+		assertTrue("Les valeurs du resultat de l'union devrait etre en ordre croissant.", resultatUnion.get(0).hashCode() < resultatUnion.get(resultatUnion.size() - 1).hashCode());
 	}
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: UNION
+	 * 		- Tri		: Decroissant
+	 * La liste chainee devrait avoir des valeurs decroissantes.
+	 */
+	@Test
+	public void test2() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(1, listeEntiers1, listeEntiers2, false);
+		
+		assertNotNull("Le resultat ne devrait pas etre null.", resultat);
+		assertEquals("Le resultat devrait contenir trois tableaux.", 3, resultat.getSize());
+		
+		ArrayList<Object> resultatUnion = resultat.getAt(2);
+		assertTrue("Les valeurs du resultat de l'union devrait etre en ordre decroissant.", resultatUnion.get(0).hashCode() > resultatUnion.get(resultatUnion.size() - 1).hashCode());
+	}
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: INTERSECTION
+	 * 		- Tri		: Croissant
+	 * La liste chainee devrait avoir des valeurs croissantes.
+	 */
+	@Test
+	public void test3() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(2, listeEntiers1, listeEntiers2, true);
+		
+		assertNotNull("Le resultat ne devrait pas etre null.", resultat);
+		assertEquals("Le resultat devrait contenir trois tableaux.", 3, resultat.getSize());
+		
+		ArrayList<Object> resultatUnion = resultat.getAt(2);
+		assertTrue("Les valeurs du resultat de l'union devrait etre en ordre croissant.", resultatUnion.get(0).hashCode() < resultatUnion.get(resultatUnion.size() - 1).hashCode());
+	}
+	
+	
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: IS_SUBSET
+	 * 		- Tri		: Croissant
+	 * La liste chainee devrait n'avoir aucune donnees concernant l'operateur.
+	 */
+	@Test()
+	public void test9() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(5, listeEntiers1, listeEntiers2, true);
+
+		assertEquals("Le resultat devrait contenir deux tableaux puisque l'operateur n'existe pas.", 2, resultat.getSize());
+	}
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: IS_SUBSET
+	 * 		- Tri		: Decroissant
+	 * La liste chainee devrait n'avoir aucune donnees concernant l'operateur.
+	 */
+	@Test()
+	public void test10() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(5, listeEntiers1, listeEntiers2, false);
+
+		assertEquals("Le resultat devrait contenir deux tableaux puisque l'operateur n'existe pas.", 2, resultat.getSize());
+	}
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: IS_SUPERSET
+	 * 		- Tri		: Croissant
+	 * La liste chainee devrait n'avoir aucune donnees concernant l'operateur.
+	 */
+	@Test()
+	public void test11() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(6, listeEntiers1, listeEntiers2, true);
+
+		assertEquals("Le resultat devrait contenir deux tableaux puisque l'operateur n'existe pas.", 2, resultat.getSize());
+	}
+	
+	/**
+	 * Classe : LinkedList
+	 * Methode : build
+	 * 		- Operator 	: IS_SUPERSET
+	 * 		- Tri		: Decroissant
+	 * La liste chainee devrait n'avoir aucune donnees concernant l'operateur.
+	 */
+	@Test()
+	public void test12() throws IOException {
+		MyListInterface resultat = uneListeChainee.build(6, listeEntiers1, listeEntiers2, false);
+
+		assertEquals("Le resultat devrait contenir deux tableaux puisque l'operateur n'existe pas.", 2, resultat.getSize());
+	}
+	
+//	StringBuilder stringContent = new StringBuilder();
+//	
+//	for(int i = 0; i < resultat.getSize(); i++){
+//		stringContent.append(" " + resultat.getAt(i));
+//	}	
+//	
+//	System.out.println("My List : " + stringContent);
 
 	// ============== TESTS DE LA CLASSE SETCALCULATOR ==============
 	
