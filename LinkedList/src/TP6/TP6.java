1a17d904fee36af0b2081086a757604b2dd818a0
Sebastien Cadorette <cado.94@me.com>
2018-12-03 22:18:23 -0500

    setUp done

--- LinkedList/src/TP6/TP6.java
+++ LinkedList/src/TP6/TP6.java
@@ -2,14 +2,42 @@ package TP6;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.LinkedList;
+import main.LinkedListInterface;
+import main.MyList;
+import main.MyListInterface;
+
 public class TP6 {
 
+	MyListInterface uneListe;
+	LinkedListInterface uneListeChainee;
+	ArrayList<Object> listeEntiers1;
+	ArrayList<Object> listeEntiers2;
+	
 	@Before
 	public void setUp() throws Exception {
+		uneListe = new MyList();
+		uneListeChainee = new LinkedList();
+		listeEntiers1 = new ArrayList<Object>();
+		listeEntiers2 = new ArrayList<Object>();
+		
+		listeEntiers1.add(10);
+		listeEntiers1.add(25);
+		listeEntiers1.add(4);
+		listeEntiers1.add(1);
+		listeEntiers1.add(1);
+		listeEntiers1.add(2);
+		
+		listeEntiers2.add(1);
+		listeEntiers2.add(1);
+		listeEntiers2.add(5);
+		listeEntiers2.add(5);
 	}
 
 	@After
@@ -17,9 +45,10 @@ public class TP6 {
 	}
 
 	// ============== TESTS DE LA CLASSE LINKEDLIST ==============
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(6, listeEntiers1.size());
 	}
 
 	// ============== TESTS DE LA CLASSE SETCALCULATOR ==============
